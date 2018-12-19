12172af3177e078c8cd5e566cecdbd72fd792c6a
Nils <nilsnathorstwindahl@gmail.com>
2018-12-19 12:36:44 +0100

    Site Under Construction

--- register.php
+++ register.php
@@ -1,6 +1,10 @@
 <?php declare(strict_types=1); ?>
 
 
-<form class="signup-form" action="index.html" method="post">
-	<input type="text" name="" value="" placeholder="Input here" required>
+<form class="register-form" action="register.php" method="post">
+	<input type="text" name="firstname" value="" placeholder="firstname" required>
+	<input type="text" name="lastname" value="" placeholder="lastname" required>
+	<input type="text" name="email" value="" placeholder="email" required>
+	<input type="text" name="password" value="" placeholder="password" required>
+	<input type="text" name="confirmpassword" value="" placeholder="confirmpassword" required>
 </form>
