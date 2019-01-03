860defa49456ea7f26df8dbf050940f7ab223e46
Nils <nilsnathorstwindahl@gmail.com>
2019-01-03 20:16:13 +0100

    update

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -1,5 +1,3 @@
-
-//thank you stackoverflow :)
 function getCookie(cookieName) {
   var name = cookieName + "=";
   var decodedCookie = decodeURIComponent(document.cookie);
@@ -9,6 +7,7 @@ function getCookie(cookieName) {
     while (c.charAt(0) == ' ') {
       c = c.substring(1);
     }
+// c.indexOf() function is used to find the index of the first occurrence of the search element provided as the argument to the function
     if (c.indexOf(name) == 0) {
       return c.substring(name.length, c.length);
     }
@@ -19,6 +18,8 @@ function getCookie(cookieName) {
 const posts = JSON.parse(getCookie('posts'))
 const userPosts = JSON.parse(getCookie('userPosts'))
 
+console.log(posts)
+console.log(userPosts)
 
 console.table(posts)
 console.table(userPosts)
