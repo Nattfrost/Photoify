dd6bfb793ce971aee715a9d758eb8d7b09086760
Nils <nilsnathorstwindahl@gmail.com>
2019-01-13 16:15:07 +0100

    css

--- assets/js/test.js
+++ assets/js/test.js
@@ -13,9 +13,9 @@ const hideLikeButtons = (json, elts) => json.map(post => {
 })
 const createDeleteButtons = (elts) => {
 	elts.map(el => {
-		if	(el.dataset.id !== getUser('user_id'))
-		el.classList.add('hidden')
-		
+		if (el.dataset.id !== getUser('user_id'))
+			el.classList.add('hidden')
+
 	})
 }
 
@@ -88,7 +88,7 @@ const handleClickComment = (event) => {
 	}, 20)
 }
 const handleClickDelete = (event) => {
-    let postId = event.target.dataset.postid
+	let postId = event.target.dataset.postid
 	document.cookie = "delete=" + postId
 	window.location.reload()
 }
@@ -100,52 +100,48 @@ const createPost = (json) => {
 		}).join('')
 
 		return `
-		<section class="feed-item">
-		<form class="deletePost-form" target="hiddenFrame" action="../app/posts/update.php" method="post">
+<section class="feed-item">
+	<form class="deletePost-form" target="hiddenFrame" action="../app/posts/update.php" method="post">
 		<button name="deletePost" data-id="${post.user_id}" data-postid="${post.post_id}" class="delete-button" type="submit">Delete Toast</button>
-		</form>
-	<div class="image-wrapper">
+	</form>
+		<div class="image-wrapper">
 		<img data-id="${post.post_id}" class="feed-image" src="${post.image}"/>
-	</div>
+		</div>
 	<div data-id="${post.post_id}" class="post-footer">
-		
 		<form class="likes-form hide-submit" action="../app/posts/likes.php" target="hiddenFrame" method="post">
-		<label>
+			<label>
 			<input name="like" type="submit" data-id="${post.post_id}" class="like like-button" />
-			<svg data-id="${post.post_id}" class="like-icon" viewBox="0 0 30 30">
-				<path d="M51.911 16.242c-.759-8.354-6.672-14.415-14.072-14.415-4.93 0-9.444 2.653-11.984 6.905-2.517-4.307-6.846-6.906-11.697-6.906C6.759 1.826.845 7.887.087 16.241c-.06.369-.306 2.311.442 5.478 1.078 4.568 3.568 8.723 7.199 12.013l18.115 16.439 18.426-16.438c3.631-3.291 6.121-7.445 7.199-12.014.748-3.166.502-5.108.443-5.477zm-2.39 5.019c-.984 4.172-3.265 7.973-6.59 10.985L25.855 47.481 9.072 32.25c-3.331-3.018-5.611-6.818-6.596-10.99-.708-2.997-.417-4.69-.416-4.701l.015-.101c.65-7.319 5.731-12.632 12.083-12.632 4.687 0 8.813 2.88 10.771 7.515l.921 2.183.921-2.183c1.927-4.564 6.271-7.514 11.069-7.514 6.351 0 11.433 5.313 12.096 12.727.002.016.293 1.71-.415 4.707z"/>
+			<svg data-id="${post.post_id}" class="like-icon" viewBox="0 0 60 60">
+				<path d="M51.911 16.242c-.759-8.354-6.672-14.415-14.072-14.415-4.93 0-9.444 2.653-11.984 6.905-2.517-4.607-6.846-6.906-11.697-6.906C6.759 1.826.845 7.887.087 16.241c-.06.369-.606 2.311.442 5.478 1.078 4.568 3.568 8.723 7.199 12.013l18.115 16.439 18.426-16.438c3.631-3.291 6.121-7.445 7.199-12.014.748-3.166.502-5.108.443-5.477zm-2.39 5.019c-.984 4.172-3.265 7.973-6.59 10.985L25.855 47.481 9.072 32.25c-3.331-3.018-5.611-6.818-6.596-10.99-.708-2.997-.417-4.69-.416-4.701l.015-.101c.65-7.319 5.731-12.632 12.083-12.632 4.687 0 8.813 2.88 10.771 7.515l.921 2.183.921-2.183c1.927-4.564 6.271-7.514 11.069-7.514 6.351 0 11.433 5.313 12.096 12.727.002.016.293 1.71-.415 4.707z"/>
 				<path d="M15.999 7.904c-5.514 0-10 4.486-10 10 0 .553.447 1 1 1s1-.447 1-1c0-4.411 3.589-8 8-8 .553 0 1-.447 1-1s-.448-1-1-1z"/>
-	 		 </svg>
-	  </label>
-
-
-		<label>
-			<input name="dislike" type="submit" data-id="${post.post_id}" class="like dislike-button" />
-			<svg data-id="${post.post_id}" class="like-icon hidden" viewBox="0 0 30 30">
-			<path d="M51.911 16.242c-.759-8.354-6.672-14.415-14.072-14.415-4.93 0-9.444 2.653-11.984 6.905-2.517-4.307-6.846-6.906-11.697-6.906C6.759 1.826.845 7.887.087 16.241c-.06.369-.306 2.311.442 5.478 1.078 4.568 3.568 8.723 7.199 12.013l18.115 16.439 18.426-16.438c3.631-3.291 6.121-7.445 7.199-12.014.748-3.166.502-5.108.443-5.477zM15.999 9.904c-4.411 0-8 3.589-8 8 0 .553-.447 1-1 1s-1-.447-1-1c0-5.514 4.486-10 10-10 .553 0 1 .447 1 1s-.448 1-1 1z"/>
-	 		 </svg>
-	  </label>
+				</svg>
+			</label>
+			<label>
+				<input name="dislike" type="submit" data-id="${post.post_id}" class="like dislike-button" />
+				<svg data-id="${post.post_id}" class="like-icon hidden" viewBox="0 0 60 60">
+					<path d="M51.911 16.242c-.759-8.354-6.672-14.415-14.072-14.415-4.93 0-9.444 2.653-11.984 6.905-2.517-4.307-6.846-6.906-11.697-6.906C6.759 1.826.845 7.887.087 16.241c-.06.369-.306 2.311.442 5.478 1.078 4.568 3.568 8.723 7.199 12.013l18.115 16.439 18.426-16.438c3.631-3.291 6.121-7.445 7.199-12.014.748-3.166.502-5.108.443-5.477zM15.999 9.904c-4.411 0-8 3.589-8 8 0 .553-.447 1-1 1s-1-.447-1-1c0-5.514 4.486-10 10-10 .553 0 1 .447 1 1s-.448 1-1 1z"/>
+				</svg>
+			</label>
 			<p data-id="${post.post_id}" class="likes">likes: ${post.no_likes}</p>
 		</form>
-
 		<div class="author-container">
 			<img class="avatar" src="${post.avatar}">
 			<p>${post.username} </p> 
 			<p class="description">${post.description}</p>
 			<p class="created-at text-uppercase">${post.created_at}</p>
 		</div>
-			<div data-id="${post.post_id}" class="comments-container">
-				<div class="comments-section" data-id="${post.post_id}">
-					${comments}
-				</div>
-	</div>
-				<form class="comments-form" action="../app/posts/comments.php" target="hiddenFrame" method="post">
-					<input type="text" name="comment" placeholder="" class="comments-input" required>
-					<button type="submit" data-id="${post.post_id}" class="comment-button">comment</button>
-				</form>
-			</div>
-		</section>
-		`
+		<div data-id="${post.post_id}" class="comments-container">
+			<div class="comments-section" data-id="${post.post_id}">
+			${comments}
+		</div>
+		</div>
+		<form class="comments-form" action="../app/posts/comments.php" target="hiddenFrame" method="post">
+			<input type="text" name="comment" placeholder="" class="comments-input" required>
+			<button type="submit" data-id="${post.post_id}" class="comment-button">comment</button>
+		</form>
+		</div>
+</section>
+`
 	}).join('')
 	container.innerHTML = postsMarkup
 
@@ -156,9 +152,9 @@ const showPostContent = (e) => {
 	let postContents = [...document.querySelectorAll('.post-footer')]
 	// currentPost = postContents.filter()
 	postContents.map(postContent => {
-		 posts = postContent.dataset.id
-		 const current = posts => posts.filter(post => post.dataset.id === e.target.dataset.id)
-		 current(postContents)[0].setAttribute('style', 'display: flex')
+		posts = postContent.dataset.id
+		const current = posts => posts.filter(post => post.dataset.id === e.target.dataset.id)
+		current(postContents)[0].setAttribute('style', 'display: flex')
 	})
 
 }
@@ -168,7 +164,7 @@ getData(url)
 			let currentUser = getUser('user_id')
 			const userfilter = data => data.filter(user => user.user_id === currentUser)
 			data = userfilter(data)
-	
+
 		}
 		createPost(data)
 		const buttons = document.querySelectorAll('.like')
@@ -190,7 +186,7 @@ getData(url)
 			const profilePosts = [...document.querySelectorAll('.feed-image')]
 
 			initEventListeners(profilePosts, showPostContent)
-		
+
 		}
 
 	})
