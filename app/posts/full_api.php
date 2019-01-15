02ab265ed150e94f4b301a0487e551f0faa06076
Nils <nilsnathorstwindahl@gmail.com>
2019-01-15 14:14:55 +0100

    more styling

--- app/posts/full_api.php
+++ app/posts/full_api.php
@@ -7,4 +7,3 @@ require __DIR__.'/../autoload.php';
 $posts = getPosts($pdo);
 
 echo json_encode($posts);
-
--- post.php
+++ post.php
@@ -6,10 +6,21 @@ require __DIR__.'/navbar.php';
 
 <h1>post</h1>
 
-<form action="/app/posts/store.php" method="POST" enctype="multipart/form-data" class="post-form">
-<input class="file-input" type="file"  onchange="previewFile()" name="image" id="image" type="file" multiple="">
+
+		<form action="/app/posts/store.php" method="POST" enctype="multipart/form-data" class="post-form">
+    <input class="file-input" type="file"  onchange="previewFile()" name="image" id="image" type="file" multiple="">
+    <br>
+    <img class="previewimage" src="" height="200">
+    <br>
+    <div class="select-image-div">
+    <input type="button" class="button image-button" value="Select image" onclick="document.getElementById('image').click();"/>
+    <br>
+        <input type="text" name="description" placeholder="add description">
         <br>
-        <img class="previewimage" src="" height="150" alt="Image preview...">
+        <button type="submit" class="button" name="button">Submit</button>
+    </form>
+
+
 	<textarea name="description" id="description" placeholder="description"></textarea>
 	<textarea name="tags" id="tags" placeholder="add tags"></textarea>
 	<button type="submit" class="">Submit Toast</button>
@@ -20,3 +31,9 @@ require __DIR__.'/navbar.php';
 require __DIR__.'/views/footer.php';
 require __DIR__.'/footer.php';
 ?>
+
+<!-- 
+<form action="/app/posts/store.php" method="POST" enctype="multipart/form-data" class="post-form">
+<input class="file-input" type="file"  onchange="previewFile()" name="image" id="image" type="file" multiple="">
+        <br>
+        <img class="previewimage" src="" height="150" alt="Image preview..."> -->
