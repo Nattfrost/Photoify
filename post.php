cb883daa4e4bd9e6a477a0cc6253986b89769fef
Nils <nilsnathorstwindahl@gmail.com>
2019-01-16 16:10:08 +0100

    trying to save this project

--- post.php
+++ post.php
@@ -3,11 +3,7 @@ declare(strict_types=1);
 require __DIR__.'/views/header.php';
 require __DIR__.'/navbar.php';
 ?>
-
-<h1>post</h1>
-
-
-		<form action="/app/posts/store.php" method="POST" enctype="multipart/form-data" class="post-form">
+	<form action="/app/posts/store.php" method="POST" enctype="multipart/form-data" class="post-form">
     <input class="file-input" type="file"  onchange="previewFile()" name="image" id="image" type="file" multiple="">
     <br>
     <img class="previewimage" src="" height="200">
@@ -15,15 +11,9 @@ require __DIR__.'/navbar.php';
     <div class="select-image-div">
     <input type="button" class="button image-button" value="Select image" onclick="document.getElementById('image').click();"/>
     <br>
-        <input type="text" name="description" placeholder="add description">
-        <br>
-        <button type="submit" class="button" name="button">Submit</button>
-    </form>
-
-
-	<textarea name="description" id="description" placeholder="description"></textarea>
-	<textarea name="tags" id="tags" placeholder="add tags"></textarea>
-	<button type="submit" class="">Submit Toast</button>
+	<input name="description" id="description" placeholder="description"/>
+	<input name="tags" id="tags" placeholder="add tags"/>
+	<button type="submit" class="button" name="button">Submit Toast</button>
 </form>
 
 
